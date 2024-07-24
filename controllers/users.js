84e17bda01da83f486f717adb94a6b7c8574c2e9
shubham Patil <shubhamkpatil474@gmail.com>
2024-07-24 11:00:12 +0530

    Add Project Files

---
+++ controllers/users.js
@@ -0,0 +1,44 @@
+const { ModifiedPathsSnapshot } = require("mongoose");
+const User = require("../models/user");
+
+module.exports.renderSignupForm = (req, res) => {
+  res.render("users/signup.ejs");
+};
+
+module.exports.signup = async (req, res) => {
+  try {
+    let { username, email, password } = req.body;
+    const newUser = new User({ email, username });
+    const registerdUser = await User.register(newUser, password);
+    req.login(registerdUser, (err) => {
+      if (err) {
+        return next(err);
+      }
+      req.flash("success", "Welcome to Traveller!");
+      res.redirect("/listings");
+    });
+  } catch (e) {
+    req.flash("error", e.message);
+    res.redirect("/signup");
+  }
+};
+
+module.exports.renderLoginForm = (req, res) => {
+  res.render("users/login.ejs");
+};
+
+module.exports.login = async (req, res, next) => {
+  req.flash("success", "Welcome back to  Traveller!");
+  let redirectUrl = res.locals.redirectUrl || "/listings";
+  res.redirect(redirectUrl);
+};
+
+module.exports.logout = (req, res, next) => {
+  req.logOut((err) => {
+    if (err) {
+      return next(err);
+    }
+    req.flash("success", "you are logged out!");
+    res.redirect("/listings");
+  });
+};
